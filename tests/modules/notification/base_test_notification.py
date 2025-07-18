fb126d99cc7f48539c3b20d7563cd844bc8ef450
Sarthak Gaikwad <sarthak.gaikwad@bettrhq.com>
2025-07-18 15:30:34 +0530

    Added AccountRepository deletion in teardown_method

--- tests/modules/notification/base_test_notification.py
+++ tests/modules/notification/base_test_notification.py
@@ -1,13 +1,10 @@
 import unittest
 from typing import Callable
 
+from modules.account.internal.store.account_repository import AccountRepository
 from modules.logger.logger_manager import LoggerManager
-from modules.notification.internals.store.device_token_repository import (
-    DeviceTokenRepository,
-)
-from modules.notification.rest_api.notification_rest_api_server import (
-    NotificationRestApiServer,
-)
+from modules.notification.internals.store.device_token_repository import DeviceTokenRepository
+from modules.notification.rest_api.notification_rest_api_server import NotificationRestApiServer
 
 
 class BaseTestNotification(unittest.TestCase):
@@ -19,3 +16,4 @@ class BaseTestNotification(unittest.TestCase):
     def teardown_method(self, method: Callable) -> None:
         print(f"Executed:: {method.__name__}")
         DeviceTokenRepository.collection().delete_many({})
+        AccountRepository.collection().delete_many({})
