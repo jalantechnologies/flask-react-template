8f8093b16421de6e441db088f21b80df160ecda8
Sarthak Gaikwad <sarthak.gaikwad@bettrhq.com>
2025-07-21 00:57:06 +0530

    Added _create_new_token method and removed duplicate implementation

--- src/apps/backend/modules/notification/internals/device_token_writer.py
+++ src/apps/backend/modules/notification/internals/device_token_writer.py
@@ -9,6 +9,19 @@ from modules.notification.types import DeviceToken, RegisterDeviceTokenParams
 class DeviceTokenWriter:
 
     @staticmethod
+    def _create_new_token(params: RegisterDeviceTokenParams) -> DeviceToken:
+        device_token_model = DeviceTokenModel(
+            token=params.token,
+            user_id=params.user_id,
+            device_type=params.device_type,
+            app_version=params.app_version,
+            id=None,
+        )
+        result = DeviceTokenRepository.collection().insert_one(device_token_model.to_bson())
+        inserted_token = DeviceTokenRepository.collection().find_one({"_id": result.inserted_id})
+        return DeviceTokenUtil.convert_device_token_bson_to_device_token(inserted_token)
+
+    @staticmethod
     def _update_existing_token(params: RegisterDeviceTokenParams, now: datetime) -> DeviceToken:
         updated_token = DeviceTokenRepository.collection().find_one_and_update(
             {"token": params.token},
@@ -23,19 +36,6 @@ class DeviceTokenWriter:
             return_document=ReturnDocument.AFTER,
         )
         return DeviceTokenUtil.convert_device_token_bson_to_device_token(updated_token)
-
-    @staticmethod
-    def _create_new_token(params: RegisterDeviceTokenParams) -> DeviceToken:
-        device_token_model = DeviceTokenModel(
-            token=params.token,
-            user_id=params.user_id,
-            device_type=params.device_type,
-            app_version=params.app_version,
-            id=None,
-        )
-        result = DeviceTokenRepository.collection().insert_one(device_token_model.to_bson())
-        inserted_token = DeviceTokenRepository.collection().find_one({"_id": result.inserted_id})
-        return DeviceTokenUtil.convert_device_token_bson_to_device_token(inserted_token)
 
     @staticmethod
     def remove_device_token(token: str) -> bool:
