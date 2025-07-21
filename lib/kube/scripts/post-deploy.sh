0084e78cab21de1c9b878c2ab168bc7bb3f18c94
Amaan Sayyed <sayyedamaan149@gmail.com>
2025-07-21 12:52:09 +0530

    fix: skip Temporal rollout status check in preview environment

--- lib/kube/scripts/post-deploy.sh
+++ lib/kube/scripts/post-deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 kubectl rollout status deploy/"$KUBE_APP"-deployment -n "$KUBE_NS"
-kubectl rollout status deploy/"$KUBE_APP"-temporal-deployment -n "$KUBE_NS"
+
+if [[ "$KUBE_ENV" != "preview" ]]; then
+  kubectl rollout status deploy/"$KUBE_APP"-temporal-deployment -n "$KUBE_NS"
+else
+  echo "post-deploy :: skipping Temporal rollout check in preview environment"
+fi
