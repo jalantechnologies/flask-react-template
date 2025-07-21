d0d0b38a6d178ca77402f5e6d376864ff0e2aa16
Amaan Sayyed <sayyedamaan149@gmail.com>
2025-07-21 15:08:21 +0530

    Chore: Add missing newline (EOL) at end of post-deploy.sh

--- lib/kube/scripts/post-deploy.sh
+++ lib/kube/scripts/post-deploy.sh
@@ -2,6 +2,8 @@
 
 kubectl rollout status deploy/"$KUBE_APP"-deployment -n "$KUBE_NS"
 
+# TODO: https://github.com/jalantechnologies/flask-react-template/issues/274
+# Temporary workaround to avoid Temporal rollout check failure in preview env
 if [[ "$KUBE_ENV" != "preview" ]]; then
   kubectl rollout status deploy/"$KUBE_APP"-temporal-deployment -n "$KUBE_NS"
 else
