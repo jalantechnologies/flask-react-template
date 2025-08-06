bdf0b8df58ed9fb4c4a2c512e7ec387f04d91ce5
AyushS104 <viveksinghhfzb95@gmail.com>
2025-08-06 23:07:20 +0530

    Update index.tsx

--- src/apps/frontend/routes/index.tsx
+++ src/apps/frontend/routes/index.tsx
@@ -1,17 +1,3 @@
-import { createBrowserRouter } from '@datadog/browser-rum-react/react-router-v6';
-import React from 'react';
-import { RouterProvider } from 'react-router-dom';
+import TaskDetail from "../pages/dashboard/TaskDetail";
 
-import { useAuthContext } from 'frontend/contexts';
-import { protectedRoutes } from 'frontend/routes/protected';
-import { publicRoutes } from 'frontend/routes/public';
-
-export const AppRoutes = () => {
-  const { isUserAuthenticated } = useAuthContext();
-
-  const routes = isUserAuthenticated() ? protectedRoutes : publicRoutes;
-
-  const router = createBrowserRouter(routes);
-
-  return <RouterProvider router={router} />;
-};
+<Route path="/dashboard/task/:id" element={<TaskDetail />} />
