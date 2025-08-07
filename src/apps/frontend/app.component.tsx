3d2156907c7e43cc69c9868f6286803c52485fd3
ROHIT MANNA <128728616+Crazy-Rohit@users.noreply.github.com>
2025-08-07 23:36:19 +0530

    Update app.component.tsx

--- src/apps/frontend/app.component.tsx
+++ src/apps/frontend/app.component.tsx
@@ -1,6 +1,7 @@
 import { ErrorBoundary } from '@datadog/browser-rum-react';
 import React, { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { ErrorFallback } from './pages/error';
 import { Logger } from './utils/logger';
@@ -8,15 +9,19 @@ import { Logger } from './utils/logger';
 import { AccountProvider } from 'frontend/contexts';
 import { AuthProvider } from 'frontend/contexts/auth.provider';
 import { Config } from 'frontend/helpers';
-import { AppRoutes } from 'frontend/routes';
 import InspectLet from 'frontend/vendor/inspectlet';
+
+// Import your comment page
+import CommentManager from 'frontend/pages/comments/CommentManager';
+
+// Import other routes (assuming AppRoutes contains default routes)
+import { AppRoutes } from 'frontend/routes';
 
 Logger.init();
 
 export default function App(): React.ReactElement {
   useEffect(() => {
     const inspectletKey = Config.getConfigValue('inspectletKey');
-
     if (inspectletKey) {
       InspectLet();
     }
@@ -27,7 +32,15 @@ export default function App(): React.ReactElement {
       <AuthProvider>
         <AccountProvider>
           <Toaster />
-          <AppRoutes />
+          <BrowserRouter>
+            <Routes>
+              {/* Default App routes */}
+              <Route path="/*" element={<AppRoutes />} />
+
+              {/* New Comment Manager route */}
+              <Route path="/comments" element={<CommentManager />} />
+            </Routes>
+          </BrowserRouter>
         </AccountProvider>
       </AuthProvider>
     </ErrorBoundary>
